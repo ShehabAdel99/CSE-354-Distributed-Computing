27881890ecdaa222130bd060a431f9ad8335d95b
Aboreda123 <m.elshafaie123@gmail.com>
2023-06-12 23:51:15 +0300

    added ready state

--- projectDis/server.py
+++ projectDis/server.py
@@ -8,12 +8,13 @@ port=5555
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 def read_pos(str):
-    str=str.split(",")
-    return int(str[0]), int(str[1]),int(str[2])
+     str=str.split(",")
+     return int(str[0]), int(str[1]), int(str[2]), int(str[3])
+
 
 def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])+ "," + str(tup[2])
-pos=[(50,500,0),(200,500,0)]
+    return str(tup[0]) + "," + str(tup[1]) + "," + str(tup[2]) + "," + str(tup[3])
+pos=[(50,500,0,0),(200,500,0,0)]
 try:
     s.bind((server,port))
 except socket.error as e:
@@ -26,6 +27,7 @@ def threaded_client(conn,player):
     conn.send(str.encode(make_pos(pos[player])))
     reply=""
 
+
     while True:
         try:
             data=read_pos(conn.recv(2048).decode())
@@ -36,10 +38,13 @@ def threaded_client(conn,player):
                 break
             else:
                 if player ==1:
+                    pos[1] = (*pos[1][:3], 1)
                     reply = pos[0]
 
 
+
                 else:
+                    pos[0] = (*pos[0][:3], 1)
                     reply = pos[1]
 
                 print("Received :",reply)
@@ -57,6 +62,7 @@ def threaded_client(conn,player):
 currentPlayer = 0
 while True:
  conn,addr=s.accept()
+
  print("Connected to:",addr)
  start_new_thread(threaded_client,(conn,currentPlayer))
  currentPlayer +=1
