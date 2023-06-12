6233a9ef406d78e13f58c83ffb23195f84f69adb
Aboreda123 <m.elshafaie123@gmail.com>
2023-06-12 18:52:25 +0300

    main game

---
+++ projectDis/server.py
@@ -0,0 +1,62 @@
+import socket
+from _thread import *
+import sys
+
+
+server="192.168.1.9"
+port=5555
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+def read_pos(str):
+    str=str.split(",")
+    return int(str[0]), int(str[1]),int(str[2])
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])+ "," + str(tup[2])
+pos=[(50,500,0),(200,500,0)]
+try:
+    s.bind((server,port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for a connection")
+
+def threaded_client(conn,player):
+    conn.send(str.encode(make_pos(pos[player])))
+    reply=""
+
+    while True:
+        try:
+            data=read_pos(conn.recv(2048).decode())
+            pos[player]=data
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                if player ==1:
+                    reply = pos[0]
+
+
+                else:
+                    reply = pos[1]
+
+                print("Received :",reply)
+                print("Sending :", reply)
+
+            conn.sendall(str.encode(make_pos(reply)))
+        except:
+            break
+
+    print("lost connection")
+    conn.close()
+
+
+
+currentPlayer = 0
+while True:
+ conn,addr=s.accept()
+ print("Connected to:",addr)
+ start_new_thread(threaded_client,(conn,currentPlayer))
+ currentPlayer +=1
