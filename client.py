e547f487ba1148b790c7bc6958d4e9afe67d4ca8
Amr Essam <amrressamm148@gmail.com>
2023-06-12 20:42:14 +0300

    connect buttons
    trying to connect main window with the game for each client

--- client.py
+++ client.py
@@ -2,17 +2,28 @@ import pygame
 from network import network
 import random
 from time import sleep
+from button import Button
+from pygame.locals import *
+pygame.init()
 
 width_dis = 360
 height_dis = 650
 win = pygame.display.set_mode((width_dis, height_dis))
 pygame.display.set_caption("Client")
 vel = 1
+pygame.font.init()
 clientNumber = 0
-crash1=0
-crash2=0
-ready1=0
-ready2=0
+screen_width=500
+screen_height=500
+screen = pygame.display.set_mode((screen_width, screen_height))
+font = pygame.font.SysFont("arial black", 40)
+TEXT_COL = (255, 255, 255)
+
+start_button=pygame.image.load(r"C:\Users\Dell\Desktop\pygame_tutorials-main\Button\start_btn.png")
+exit_button=pygame.image.load(r"C:\Users\Dell\Desktop\pygame_tutorials-main\Button\exit_btn.png")
+
+#start_click=Button(100,200,start_button)
+#exit_click=Button(100,200,exit_button)
 class Player():
     def __init__(self, x, y, width, height, car_image, bg_image):
         pygame.init()
@@ -34,7 +45,7 @@ class Player():
         self.bg_img_y2 = -600
         self.bg_img_speed = 0.7
         self.count = 0
-        self.enemy_car = pygame.image.load(r"C:\Users\Dell\Desktop\Distributed project\img\WhatsApp Image 2023-06-11 at 2.29.02 AM.jpeg")
+        self.enemy_car = pygame.image.load(r"C:\Users\Dell\Desktop\projectDis\img\enemy_car_1.png")
         self.enemy_car_startx = random.randrange(100, 360)
         self.enemy_car_starty = -600
         self.enemy_car_speed = 0.5
@@ -63,7 +74,6 @@ class Player():
         win.blit(self.car_image, (x, y))
 
     def move(self, win):
-        global crash1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.crashed = True
@@ -82,7 +92,7 @@ class Player():
         if keys[pygame.K_DOWN]:
             self.y += self.vel
 
-        self.update(win)
+        self.update()
         self.back_ground_raod(win)
         self.run_enemy_car(self.enemy_car_startx, self.enemy_car_starty, win)
         self.enemy_car_starty += self.enemy_car_speed
@@ -103,7 +113,6 @@ class Player():
             if self.x + 25 > self.enemy_car_startx and self.x + 25 < self.enemy_car_startx + self.enemy_car_width:
                 self.crashed = True
                 self.display_message("You lost!", win)
-                crash1 = 1
                 sleep(1)
                 self.x = 160
                 self.y = 550
@@ -115,7 +124,6 @@ class Player():
         if self.x < 20 or self.x > 300:
                 self.crashed = True
                 self.display_message("You lost!", win)
-                crash1 = 1
                 sleep(1)
                 self.x = 160
                 self.y = 550
@@ -127,7 +135,7 @@ class Player():
 
 
 
-        self.update(win)
+        self.update()
 
     def display_message(self, msg, win):
         font = pygame.font.SysFont("comicsansms", 30, True)
@@ -157,37 +165,17 @@ class Player():
         text = font.render("Score : " + str(count), True, self.white)
         win.blit(text, (0, 0))
 
-    def update(self,win):
-        global crash2
-        global crash1
+    def update(self):
         self.rect = (self.x, self.y, self.width, self.height)
-        if crash2 == 1:
-          self.display_message("you win!!",win)
-          crash2=0
-          crash1=0
-          font = pygame.font.SysFont("comicsansms", 15, True)
-
-          # Display a message on the screen
-          text = font.render("Press Any key to continue", True, (255, 255, 255))
-          win.blit(text, (200- text.get_width() / 2, 400 - text.get_height() / 2))
-          pygame.display.update()
-          pygame.event.clear()
-          pygame.event.wait()
-          while pygame.event.peek(pygame.KEYUP):
-              pygame.event.wait()
-
-
-
-
 
 
 def read_pos(str):
-    str=str.split(",")
-    return int(str[0]), int(str[1]), int(str[2])
+    str = str.split(",")
+    return int(str[0]), int(str[1])
 
 
 def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1]) + "," + str(tup[2])
+    return str(tup[0]) + "," + str(tup[1])
 
 
 def redrawWindow(win, player, player2):
@@ -195,62 +183,35 @@ def redrawWindow(win, player, player2):
     player2.draw(win)
     pygame.display.update()
 
+def draw_text(text, font, text_col, x, y):
+  img = font.render(text, True, text_col)
+  screen.blit(img, (x, y))
+
 
 def main():
-    global crash2
-    global crash1
-    global ready2
-    global ready1
     run = True
     n = network()
-    startPos = read_pos(n.getPos())
-    car_image = r"C:\Users\Dell\Desktop\Distributed project\img\WhatsApp Image 2023-06-11 at 2.29.02 AM (1).jpeg"
-    car_image2 = r"C:\Users\Dell\Desktop\Distributed project\img\WhatsApp Image 2023-06-11 at 2.29.03 AM (1).jpeg"
-    bg_img = pygame.image.load(r"C:\Users\Dell\Desktop\Distributed project\img\WhatsApp Image 2023-06-11 at 2.29.03 AM.jpeg")
+    startPos = read_pos(n.getPos())  # returns the position whether player 1 or player 2
+    car_image = r"C:\Users\Dell\Desktop\projectDis\img\car.png"
+    car_image2 = r"C:\Users\Dell\Desktop\projectDis\img\enemy_car_2.png"
+    bg_img = pygame.image.load(r"C:\Users\Dell\Desktop\projectDis\img\White-broken-lines.png")
     scaled_image = pygame.transform.scale(bg_img, (360, 650))
     p = Player(startPos[0], startPos[1], 49, 100, car_image2, scaled_image)
     p2 = Player(0, 0, 49, 100, car_image, scaled_image)
-    clock=pygame.time.Clock()
-    while run:
-      p2Pos = read_pos(n.send(make_pos((p.x, p.y, crash1))))
-      p2.x = p2Pos[0]
-      p2.y = p2Pos[1]
-      crash2 = p2Pos[2]
-      p2.update(win)
-      clock.tick(100)
-      win.fill((128, 128, 128))
-      font = pygame.font.SysFont("comicsans", 20)
-      text = font.render("Click to play!", 1, (255, 0, 0))
-      win.blit(text, (100, 200))
-      pygame.display.update()
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-             run = False
-
-        menu_screen()
-
-      p.move(win)
-      redrawWindow(win, p, p2)
-
-# first window appears
-def menu_screen():
-    run=False
-    clock=pygame.time.Clock()
+    game_paused=False
 
     while run:
-        clock.tick(60)
-        win.fill((128, 128, 128))
-        font=pygame.font.SysFont("comicsans",40)
-        text=font.render("Click to plat!",1,(255,0,0))
-        win.blit(text,(100,200))
-        pygame.display.update()
+        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
+        p2.x = p2Pos[0]
+        p2.y = p2Pos[1]
+        crash2 = p2Pos[2]
+
+        p2.update(win)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                run = False
-    main()
 
-while True:
-    menu_screen()
+        p.move(win)
+        redrawWindow(win, p, p2)
+main()
